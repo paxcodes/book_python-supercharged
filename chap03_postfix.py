4a923d3a06eb449484c37d0839295a47f42df51d
Pax <pax.margret@gmail.com>
2019-10-11 14:44:37 -0700

    Add error checking

--- chap03_postfix.py
+++ chap03_postfix.py
@@ -7,6 +7,14 @@ def evaluatePostfixExpression(expression):
      if value.isnumeric():
         stack.append(value)
      else:
+        if len(stack) < 2:
+           print("Invalid expression!")
+           break
+        
+        if value not in '+-*/':
+           print("Invalid operator: " + value)
+           break
+         
         secondOperand = str(stack.pop())
         firstOperand = str(stack.pop())
         value = eval(firstOperand + value + secondOperand)
