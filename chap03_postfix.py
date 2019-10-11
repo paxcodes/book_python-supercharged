026d48d1a69bfe15350833a3662c459dfa196f2c
Pax <pax.margret@gmail.com>
2019-10-11 14:35:45 -0700

    Evaluate postfix expression

---
+++ chap03_postfix.py
@@ -0,0 +1,15 @@
+
+def evaluatePostfixExpression(expression):
+  ops = expression.split()
+  stack = []
+
+  for value in ops:
+     if (value.isnumeric()):
+        stack.append(value)
+     else:
+        secondOperand = str(stack.pop())
+        firstOperand = str(stack.pop())
+        value = eval(firstOperand + value + secondOperand)
+        stack.append(value)
+
+  return stack.pop()
