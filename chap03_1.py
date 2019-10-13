32a94a8801a73b61d7a0e25c537c2e49cdc87566
Pax <pax.margret@gmail.com>
2019-10-12 18:13:43 -0700

    get standard deviation

--- chap03_1.py
+++ chap03_1.py
@@ -3,5 +3,9 @@ from functools import reduce
 
 if '__main__' == __name__:
    integers = [ randint(0,100) for i in range(randint(5,20))]
+   mean = reduce(lambda x,y: x+y, integers) / len(integers)
    print(integers)
-   print(reduce(lambda x,y: x+y, integers) / len(integers))
+   print(mean)
+   
+   deviation = [ (mean-i)**2 for i in integers]
+   print(deviation)
