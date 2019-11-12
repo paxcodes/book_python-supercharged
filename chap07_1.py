90b1b7a9232a3ff4f96240dfce9cab73b00038b4
Pax <pax.margret@gmail.com>
2019-11-11 20:00:23 -0800

    Match sentences
    Where sentences can have multiple spaces in-between and can start with
    a number.

--- chap07_1.py
+++ chap07_1.py
@@ -5,3 +5,13 @@ def matchSentence(text):
     pattern = r'[A-Z].*?[.!?](?= [A-Z]|$)'
     sentences = re.findall(pattern, text, flags=re.DOTALL | re.MULTILINE)
     return sentences
+
+
+def getSentences(text):
+    """
+    Sentences could have multiple spaces in-between and can start with
+    a number.
+    """
+    pattern = r'[A-Z0-9].*?[.!?](?=\s+[A-Z0-9]|$)'
+    matches = re.findall(pattern, text, flags=re.DOTALL | re.MULTILINE)
+    return matches
--- tests/test_chap07_1.py
+++ tests/test_chap07_1.py
@@ -1,4 +1,6 @@
+import re
 from chap07_1 import matchSentence
+from chap07_1 import getSentences
 
 
 def test_matchSentence1():
@@ -8,4 +10,14 @@ def test_matchSentence1():
         'See the U.S.A. today.',
         "It's right here, not a world away.",
         'Average temp. is 66.5.'
+    ]
+
+
+def test_getSentence1():
+    text = '''See the U.S.A. today.     \tIt's right here, not a world away. 66.5 is the average temp.'''
+
+    assert getSentences(text) == [
+        'See the U.S.A. today.',
+        "It's right here, not a world away.",
+        '66.5 is the average temp.'
     ]
