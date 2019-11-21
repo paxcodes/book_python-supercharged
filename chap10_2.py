72a3e2d71378c755513f40d7e6ffda8fb74caa82
Pax <pax.margret@gmail.com>
2019-11-20 21:01:42 -0800

    Define a `Money` class

---
+++ chap10_2.py
@@ -0,0 +1,13 @@
+from decimal import Decimal
+
+
+class Money(Decimal):
+
+    def __new__(cls, v, units="USD"):
+        return super(Money, cls).__new__(cls, v)
+
+    def __init__(self, v, units="USD"):
+        self.units = units
+
+    def __str__(self):
+        return super().__str__() + ' ' + {self.units}
